a0ef1d74ebe136497df3a5ec6764d0d17cfe6483
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2023-02-06 12:56:10 -0500

    do not plot by default

--- seaexplorer/run_seaexplorer.jl
+++ seaexplorer/run_seaexplorer.jl
@@ -1,4 +1,4 @@
 mission = 37; # M37 is Jan Mayen, M38 is Lofoten Basin
 include("seaexplorer_load.jl");
 include("seaexplorer_process.jl");
-include("seaexplorer_plotFast.jl")
+#include("seaexplorer_plotFast.jl")
