e16dc8500d91c226a0b4a8cd033b045d5fb724b2
Donglai Gong <truedichotomy@gmail.com>
2023-03-29 23:03:54 -0400

    Update load_maracoos_glider.jl

--- slocum/load_maracoos_glider.jl
+++ slocum/load_maracoos_glider.jl
@@ -46,7 +46,7 @@ end
 
 # specify valid data time period
 rootdir = "/Users/gong/oceansensing Dropbox/C2PO/MARACOOS/";
-datadir = rootdir * "electa-20230320-maracoos/from-glider/electa-from-glider-20230328T193841/";
+datadir = rootdir * "electa-20230320-maracoos/from-glider/electa-from-glider-20230330T024146/";
 cacdir = rootdir * "electa-20230320-maracoos/from-glider/cache/";
 t0 = DateTime("2023-03-21");
 tN = DateTime("2023-04-21");
@@ -72,7 +72,8 @@ sci_water_temp = pyrow2jlcol(dataElecta.get("sci_water_temp"));
 sci_water_cond = pyrow2jlcol(dataElecta.get("sci_water_cond"));
 sci_water_pressure = pyrow2jlcol(dataElecta.get("sci_water_pressure"));
 sci_flbbcd_chlor_units = pyrow2jlcol(dataElecta.get("sci_flbbcd_chlor_units"));
-sci_bsipar_par = pyrow2jlcol(dataElecta.get("sci_bsipar_par"));  
+sci_bsipar_par = pyrow2jlcol(dataElecta.get("sci_bsipar_par"));
+m_num_tot_inflections = pyrow2jlcol(dataElecta.get("m_tot_num_inflections")); 
 
 # QC loaded data by time and values, create interpolation function for T,C,P
 tempind = findall((trange[1] .<= sci_water_temp[:,1] .<= trange[end]) .& (40.0 .>= sci_water_temp[:,2] .> 0.0));
