a703ac8f645cf8405e457097ee4622903676738e
Donglai Gong <truedichotomy@gmail.com>
2023-04-19 12:57:53 -0400

    loading MR data in MAT format into Julia.

--- microrider/mr_load_mat.jl
+++ microrider/mr_load_mat.jl
@@ -2,6 +2,7 @@
 # 2023-04-18 gong@vims.edu
 
 using Glob, MAT
+import MR_types: MicroRiderRaw
 
 datadirJM = "/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221021-norse-janmayen-complete/mr1000g/"
 datadirLBE = "/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221102-norse-lofoten-complete/mr1000g/"
@@ -9,7 +10,67 @@ datadirLBE = "/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-202
 datadir = datadirJM;
 datafiles = Glob.glob("*.mat", datadir);
 
-file = matopen(datafiles[1])
-a = read(file, "Gnd")
-close(file)
+global mrdata = MicroRiderRaw[];
 
+for i = 1:10
+    display(i)
+    mrfile = matopen(datafiles[i]);
+    fullPath = read(mrfile, "fullPath");
+    fs_fast = read(mrfile, "fs_fast");
+    fs_slow = read(mrfile, "fs_slow");
+    header_version = read(mrfile, "header_version")
+    t_slow = read(mrfile, "t_slow")
+    t_fast = read(mrfile, "t_fast")
+    setupfilestr = read(mrfile, "setupfilestr")
+    cfgobj = read(mrfile, "cfgobj");
+    header = read(mrfile, "header");
+    filetime = read(mrfile, "filetime");
+    date = read(mrfile, "date");
+    time = read(mrfile, "time");
+    Gnd = read(mrfile, "Gnd");
+    Ax = read(mrfile, "Ax");
+    Ay = read(mrfile, "Ay");
+    T1 = read(mrfile, "T1");
+    T1_dT1 = read(mrfile, "T1_dT1");
+    T2 = read(mrfile, "T2");
+    T2_dT2 = read(mrfile, "T2_dT2");
+    sh1 = read(mrfile, "sh1");
+    sh2 = read(mrfile, "sh2");
+    P = read(mrfile, "P");
+    P_dP = read(mrfile, "P_dP");
+    PV = read(mrfile, "PV");
+    V_Bat = read(mrfile, "V_Bat");
+    Incl_Y = read(mrfile, "Incl_Y");
+    Incl_X = read(mrfile, "Incl_X");
+    Incl_T = read(mrfile, "Incl_T");
+    odas_version = read(mrfile, "odas_version");
+    vehicle_info = read(mrfile, "vehicle_info");
+    t_fast_YD = read(mrfile, "t_fast_YD");
+    t_slow_YD = read(mrfile, "t_slow_YD");
+    Year = read(mrfile, "Year");
+    Month = read(mrfile, "Month");
+    Day = read(mrfile, "Day");
+    Hour = read(mrfile, "Hour");
+    Minute = read(mrfile, "Minute");
+    Second = read(mrfile, "Second");
+    Milli = read(mrfile, "Milli");
+    T1_slow = read(mrfile, "T1_slow");
+    T1_fast = read(mrfile, "T1_fast");
+    T2_slow = read(mrfile, "T2_slow");
+    T2_fast = read(mrfile, "T2_fast");
+    P_slow = read(mrfile, "P_slow");
+    P_fast = read(mrfile, "P_fast");
+    temperature_fast = read(mrfile, "temperature_fast");
+    W_slow = read(mrfile, "W_slow");
+    W_fast = read(mrfile, "W_fast");
+    speed_slow = read(mrfile, "speed_slow");
+    speed_fast = read(mrfile, "speed_fast");
+    gradT1 = read(mrfile, "gradT1");
+    gradT2 = read(mrfile, "gradT2");
+    input_parameters = read(mrfile, "input_parameters");
+    params = read(mrfile, "params");
+
+    mrprofile = MicroRiderRaw(fullPath, fs_fast, fs_slow, header_version, t_slow, t_fast, setupfilestr, cfgobj, header, filetime, date, time, Gnd, Ax, Ay, T1, T1_dT1, T2, T2_dT2, sh1, sh2, P, P_dP, PV, V_Bat, Incl_Y, Incl_X, Incl_T, odas_version, vehicle_info, t_fast_YD, t_slow_YD, Year, Month, Day, Hour, Minute, Second, Milli, T1_slow, T1_fast, T2_slow, T2_fast, P_slow, P_fast, temperature_fast, W_slow, W_fast, speed_slow, speed_fast, gradT1, gradT2, input_parameters, params);
+    global mrdata = push!(mrdata, mrprofile);
+    close(mrfile)
+end
--- microrider/mr_types.jl
+++ microrider/mr_types.jl
@@ -1,5 +1,60 @@
-module Rockland
+module MR_types
 
-mutable struct MicroRider
-    
+mutable struct MicroRiderRaw
+    fullPath::String
+    fs_fast::AbstractFloat
+    fs_slow::AbstractFloat
+    header_version::Float64
+    t_slow::Array{Float64}
+    t_fast::Array{Float64}
+    setupfilestr::String
+    cfgobj::Dict{String, Any}
+    header::Array{Float64}
+    filetime::Float64
+    date::String
+    time::String
+    Gnd::Array{Float64}
+    Ax::Array{Float64}
+    Ay::Array{Float64}
+    T1::Array{Float64}
+    T1_dT1::Array{Float64}
+    T2::Array{Float64}
+    T2_dT2::Array{Float64}
+    sh1::Array{Float64}
+    sh2::Array{Float64}
+    P::Array{Float64}
+    P_dP::Array{Float64}
+    PV::Array{Float64}
+    V_Bat::Array{Float64}
+    Incl_Y::Array{Float64}
+    Incl_X::Array{Float64}
+    Incl_T::Array{Float64}
+    odas_version::Float64
+    vehicle_info::Dict{String, Any}
+    t_fast_YD::Array{Float64}
+    t_slow_YD::Array{Float64}
+    Year::Float64
+    Month::Float64
+    Day::Float64
+    Hour::Float64
+    Minute::Float64
+    Second::Float64
+    Milli::Float64
+    T1_slow::Array{Float64}
+    T1_fast::Array{Float64}
+    T2_slow::Array{Float64}
+    T2_fast::Array{Float64}
+    P_slow::Array{Float64}
+    P_fast::Array{Float64}
+    temperature_fast::Array{Float64}
+    W_slow::Array{Float64}
+    W_fast::Array{Float64}
+    speed_slow::Array{Float64}
+    speed_fast::Array{Float64}
+    gradT1::Array{Float64}
+    gradT2::Array{Float64}
+    input_parameters::Dict{String, Any}
+    params::Dict{String, Any}
+end
+
 end
