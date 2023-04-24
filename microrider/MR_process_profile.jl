0c206d68d8e5a6342e3f01d634c2a3cae80b167d
Donglai Gong <truedichotomy@gmail.com>
2023-04-24 15:28:22 -0400

    Update MR_process_profile.jl

--- microrider/MR_process_profile.jl
+++ microrider/MR_process_profile.jl
@@ -1,6 +1,7 @@
 using Glob, JLD2
-import MR_types: MicroRiderRaw
-import MR_io: MR_mat2jld2, MR_loadjld2, MR_datasetup, MR_load_profile
+#import MR_types: MicroRiderRaw
+#import MR_io: MR_datasetup, MR_mat2jld2, MR_load_profile
+import MR_io: MR_load_profile
 
 project = "NORSE"
 mission = "LBE"
