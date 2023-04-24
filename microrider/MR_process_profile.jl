07d74cf9524d275c161b29aeeccbaf25d167b126
Donglai Gong <truedichotomy@gmail.com>
2023-04-24 16:15:03 -0400

    removed unnecessary packages from being loaded.

--- microrider/MR_process_profile.jl
+++ microrider/MR_process_profile.jl
@@ -1,9 +1,11 @@
-using Glob, JLD2
+#using Glob, JLD2, Makie
 #import MR_types: MicroRiderRaw
 #import MR_io: MR_datasetup, MR_mat2jld2, MR_load_profile
 import MR_io: MR_load_profile
 
 project = "NORSE"
 mission = "LBE"
+profileid = 230;
 
-mrr = MR_load_profile(project, mission, 15);
+mrr = MR_load_profile(project, mission, profileid);
+
