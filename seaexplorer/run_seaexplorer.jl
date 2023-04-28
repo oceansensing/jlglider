54e54190129d0ccb3f41036e476c99b7b43e33e0
Donglai Gong <truedichotomy@gmail.com>
2023-04-28 09:22:37 -0400

    adding JM and LBE loading

--- seaexplorer/run_seaexplorer.jl
+++ seaexplorer/run_seaexplorer.jl
@@ -4,6 +4,9 @@ mission = 38; # M37 is Jan Mayen, M38 is Lofoten Basin
 #include("seaexplorer_load.jl");
 jmnav, jmnav1d, jmpld, jmpld1d = seaexplorer_load_mission(37);
 lbenav, lbenav1d, lbepld, lbepld1d = seaexplorer_load_mission(38);
-jm = seaexplorer_process(lbepld1d);
+jm = seaexplorer_process(jmpld1d);
 lbe = seaexplorer_process(lbepld1d);
+
+sea064data = jm;
+
 #include("seaexplorer_plotFast.jl")
