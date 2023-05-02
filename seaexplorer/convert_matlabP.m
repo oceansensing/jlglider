38cf07943bc558dd103133580c0eb5658459729c
Donglai Gong <truedichotomy@gmail.com>
2023-05-02 11:38:50 -0700

    code for loading of freya data

--- seaexplorer/convert_matlabP.m
+++ seaexplorer/convert_matlabP.m
@@ -1,3 +1,20 @@
+% This script loads MR data from SEA064 and data from UAF glider Freya processed by Laur Ferris.
+% Time in Matlab's datenum format is converted to unix/posix time prior to resaving the data files.
+% gong@vims.edu  2023-05-01
+
+load jm_P.mat
 dtime = datetime(dnum,'ConvertFrom','datenum','TimeZone','America/New_York');
 unixt = posixtime(dtime);
-%save lb_P -nocompression
+save jm_P -nocompression
+
+load lb_P.mat
+dtime = datetime(dnum,'ConvertFrom','datenum','TimeZone','America/New_York');
+unixt = posixtime(dtime);
+save lb_P -nocompression
+
+load freya.mat
+freya.dtime = datetime(freya.dnum, 'ConvertFrom', 'datenum', 'TimeZone', 'UTC');
+freya.unixt = posixtime(freya.dtime);
+freyanav.dtime = datetime(freyanav.dnum, 'ConvertFrom', 'datenum', 'TimeZone', 'UTC');
+freyanav.unixt = posixtime(freyanav.dtime);
+save freya.mat -nocompression
--- seaexplorer/freya_MR_laur_load.jl
+++ seaexplorer/freya_MR_laur_load.jl
@@ -3,55 +3,42 @@
 
 using MAT, GibbsSeaWater, Interpolations, NaNMath
 
-function freya_MR_laur_load(pld1, pld2, pz, dz)
+#function freya_MR_laur_load(pld1, pld2, pz, dz)
+
 
 datadir = "/Users/gong/GitHub/jlglider/seaexplorer/data/"
-freya = matopen(datadir * "freya.mat");
+glider = matread(datadir * "freya.mat");
 
-mr1_t = read(mr1, "unixt");
-mr1_p = read(mr1, "pres");
-mr1_eps1 = read(mr1, "eps1");
-mr1_eps2 = read(mr1, "eps2");
+gtime = glider["freya"]["unixt"][:];
+gpres = glider["freya"]["sci_water_pressure"][:];
+gtemp = glider["freya"]["sci_water_temp"][:];
+gcond = glider["freya"]["sci_water_cond"][:];
+gpres_tp = glider["freya"]["sci_lsltp_pressure"][:];
+gtime_tp = glider["freya"]["sci_lsltp_timestamp"][:];
+geps1_tp = glider["freya"]["sci_lsltp_e1"][:];
+geps2_tp = glider["freya"]["sci_lsltp_e2"][:];
 
-mr2_t = read(mr2, "unixt");
-mr2_p = read(mr2, "pres");
-mr2_eps1 = read(mr2, "eps1");
-mr2_eps2 = read(mr2, "eps2");
+gnavlon = glider["freyanav"]["lon"][:];
+gnavlat = glider["freyanav"]["lat"][:];
+gnavtime = glider["freyanav"]["unixt"][:];
 
-mr1_z = gsw_z_from_p.(mr1_p, 70, 0, 0);
-mr2_z = gsw_z_from_p.(mr2_p, 70, 0, 0);
+glonf = linear_interpolation(gnavtime, gnavlon, extrapolation_bc=Line());
+glatf = linear_interpolation(gnavtime, gnavlat, extrapolation_bc=Line());
 
-lon1f = linear_interpolation(pld1t, pld1.lon);
-lat1f = linear_interpolation(pld1t, pld1.lat);
-lon2f = linear_interpolation(pld2t, pld2.lon);
-lat2f = linear_interpolation(pld2t, pld2.lat); #  extrapolation_bc=Line()
+#zlo, zhi = pz-dz, pz+dz;
 
-zlo, zhi = pz-dz, pz+dz;
+glon = glonf(gtime[:]);
+glat = glatf(gtime[:]);
 
-lon1 = zeros(Float64, size(mr1_t,1), size(mr1_t,2));
-lat1 = deepcopy(lon1);
-lon2 = zeros(Float64, size(mr2_t,1), size(mr2_t,2));
-lat2 = deepcopy(lon2);
+gz = gsw_z_from_p.(gpres, 70, 0, 0);
+gsalt = gsw_sp_from_c.(gcond, gtemp, gpres);
+gsaltA = gsw_sa_from_sp.(gsalt, gpres, glon, glat);
+gctemp = gsw_ct_from_t.(gsaltA, gtemp, gpres);
+gsigma0 = gsw_sigma0.(gsaltA, gctemp);
+gspice0 = gsw_spiciness0.(gsaltA, gctemp);
+gsndspd = gsw_sound_speed.(gsaltA, gctemp, gpres);
 
-eps1 = zeros(Float64, size(mr1_t,2));
-eps2 = zeros(Float64, size(mr2_t,2));
 
-for i = 1:size(mr1_p, 2)
-    lon1[:,i] = lon1f(mr1_t[:,i]);
-    lat1[:,i] = lat1f(mr1_t[:,i]);
 
-    zind = findall(zlo .<= mr1_z[:,i] .<= zhi);
-    eps1[i] = 10 .^ NaNMath.mean(log10.(mr1_eps1[zind,i]));
-end
-
-for i = 1:size(mr2_p, 2)
-    lon2[:,i] = lon2f(mr2_t[:,i]);
-    lat2[:,i] = lat2f(mr2_t[:,i]);
-
-    zind = findall(zlo .<= mr2_z[:,i] .<= zhi);
-    eps2[i] = 10 .^ NaNMath.mean(log10.(mr2_eps1[zind,i]));
-end
-
-return lon1, lat1, lon2, lat2, eps1, eps2
-end
+#end
     
