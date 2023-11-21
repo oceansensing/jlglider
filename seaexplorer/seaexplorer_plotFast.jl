124d41a54d0c3b2873c4b024a181311b21a5f15d
Donglai Gong <truedichotomy@gmail.com>
2023-11-21 17:31:10 -0500

    Updated plotting of SEA064 section data with better clims and titles

--- seaexplorer/seaexplorer_plotFast.jl
+++ seaexplorer/seaexplorer_plotFast.jl
@@ -37,22 +37,19 @@ elseif mission == 48 # Jan Mayen 2023
     region = "JM"
     #lims_temp = (-0.5, 6.0);
     lims_temp = (-0.5, 4.5);
-    lims_salt = (34.3, 35.2);
-    lims_sigma0 = (27.3, 28.15);
+    lims_salt = (33.8, 35.2);
+    lims_sigma0 = (26.9, 28.1);
     lims_spice0 = (-0.2, 0.5);
     lims_uv = (-0.6, 0.6);
     lims_sndspd = (1450, 1475);
     lims_n2 = (0, 0.00005);
-    lims_epsilon = (-11.5, -6);
+    lims_epsilon = (-11.5, -5.5);
     t1 = Dates.DateTime(2023,11,12,0,0,0);
     t2 = Dates.DateTime(2023,11,27,0,0,0);
 end
 
 ms = 4;
 ps = (1200,600);
-
-tind = findall(t1 .<= sea064pld1d.t .<= t2);
-tindmid = findall((t1 .<= sea064pld1d.tmid .<= t2) .&& (sea064pld1d.zmid .<= -2.0));
 
 #l8out8 = @layout([a; b; c; d; e; f; g; h])
 #l8out7 = @layout([a; b; c; d; e; f; g])
@@ -72,28 +69,34 @@ sspice0 = GibbsSeaWater.gsw_spiciness0.(ssaltA, cctemp);
 revSpectral = reverse(ColorSchemes.Spectral);
 loadcolorscheme(:harpercm, revSpectral.colors, revSpectral.notes);
 
-htemp = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ctemp[tind], seriestype=:scatter, seriescolor=:harpercm, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_temp, colorbar = true, size=ps, dpi=300, framestyle=:box, title="Conservative Temperature");
-#Plots.contour!(sea064pld1d.t, sea064pld1d.z, sigma0);
-hsalt = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.saltA[tind], seriestype=:scatter, c=:haline, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_salt, colorbar = true, size=ps, dpi=300, framestyle=:box, title="Absolute Salinity")
-hsigma0 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.sigma0[tind], seriestype=:scatter, c=:dense, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sigma0, colorbar = true, size=ps, dpi=300, framestyle=:box, title="Potential Density Anomaly")
-hspice0 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.spice0[tind], seriestype=:scatter, c=:hsv, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_spice0, colorbar = true, size=ps, dpi=300, framestyle=:box, title="Spiciness")
-hsndspd = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.sndspd[tind], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sndspd, colorbar = true, size=ps, dpi=300, framestyle=:box, title="Sound Speed")
-hUeast = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ad2cp_Ueast[tind], seriestype=:scatter, c=:vik, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_uv, colorbar = true, size=ps, dpi=300, framestyle=:box, title="U: E-W Velocity")
-hUnorth = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ad2cp_Unorth[tind], seriestype=:scatter, c=:vik, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_uv, colorbar = true, size=ps, dpi=300, framestyle=:box, title="V: N-S Velocity")
+tind = findall(t1 .<= sea064pld1d.t .<= t2);
+tindmid = findall((t1 .<= sea064pld1d.tmid .<= t2) .&& (sea064pld1d.zmid .<= -2.0));
 
-hN2 = Plots.plot(sea064pld1d.tmid[tindmid], sea064pld1d.zmid[tindmid], zcolor = sea064pld1d.n2[tindmid], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_n2, colorbar = true, size=ps, dpi=300, framestyle=:box, title="N^2")
-hMReps1 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_eps1[tind]), seriestype=:scatter, c=:harpercm, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_epsilon, colorbar = true, size=ps, dpi=300, framestyle=:box, title="TKE Dissipation: Shear 1")
-hMReps2 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_eps2[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_epsilon, colorbar = true, size=ps, dpi=300, framestyle=:box, title="TKE Dissipation: Shear 2")
+htemp = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ctemp[tind], seriestype=:scatter, seriescolor=:harpercm, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_temp, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Conservative Temperature");
+#Plots.contour!(sea064pld1d.t, sea064pld1d.z, sigma0);
+hsalt = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.saltA[tind], seriestype=:scatter, c=:haline, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_salt, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Absolute Salinity")
+hsigma0 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.sigma0[tind], seriestype=:scatter, c=:dense, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sigma0, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Potential Density Anomaly")
+hspice0 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.spice0[tind], seriestype=:scatter, c=:hsv, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_spice0, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Spiciness")
+hsndspd = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.sndspd[tind], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sndspd, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Sound Speed")
+hUeast = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ad2cp_Ueast[tind], seriestype=:scatter, c=:vik, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_uv, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 U: E-W Velocity")
+hUnorth = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.ad2cp_Unorth[tind], seriestype=:scatter, c=:vik, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_uv, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 V: N-S Velocity")
+
+hN2 = Plots.plot(sea064pld1d.tmid[tindmid], sea064pld1d.zmid[tindmid], zcolor = sea064pld1d.n2[tindmid], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_n2, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 N^2")
+
+eps1gind = findall(isnan.(log10.(sea064pld1d.mr_eps1[tind])) .== 0);
+hMReps1 = Plots.plot(sea064pld1d.t[tind][eps1gind], sea064pld1d.z[tind][eps1gind], zcolor = log10.(sea064pld1d.mr_eps1[tind][eps1gind]), seriestype=:scatter, c=:harpercm, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_epsilon, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 TKE Dissipation: Shear 1")
+
+hMReps2 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_eps2[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_epsilon, colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 TKE Dissipation: Shear 2")
 hMRqc1 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.mr_qc1[tind], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="")
 hMRqc2 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.mr_qc2[tind], seriestype=:scatter, c=:jet1, markersize = ms, markerstrokewidth = 0, legend = false, label="")
-hMRsh1std = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_sh1_std[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", colorbar = true, size=ps, dpi=300, framestyle=:box, title="Shear 1 std. dev.")
-hMRsh2std = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_sh2_std[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", colorbar = true, size=ps, dpi=300, framestyle=:box, title="Shear 2 std. dev.")
+hMRsh1std = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_sh1_std[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Shear 1 std. dev.")
+hMRsh2std = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.mr_sh2_std[tind]), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", colorbar = true, size=ps, dpi=300, framestyle=:box, title="SEA064 Shear 2 std. dev.")
 
 #hchla = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.chla[tind], seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=(0, 0.6), colorbar = true, size=ps, framestyle=:box, title="Chlorophyll-a Fluorescence")
 #hbb700 = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = log10.(sea064pld1d.bb700[tind] .+ 0.0001), seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=(-5, -3), colorbar = true, size=ps, framestyle=:box, title="Backscattering (700 nm)")
 #hcdom = Plots.plot(sea064pld1d.t[tind], sea064pld1d.z[tind], zcolor = sea064pld1d.cdom[tind], seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=(0, 1.0), colorbar = true, size=ps, framestyle=:box, title="CDOM Fluorescence")
 
-hTS = Plots.plot(sea064pld1d.saltA[tind], sea064pld1d.ctemp[tind], zcolor = sea064pld1d.sigma0[tind], seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sigma0, xlims=lims_salt, ylims=lims_temp, colorbar = false, size=(1000,1000), framestyle=:box, title="CT vs SA")
+hTS = Plots.plot(sea064pld1d.saltA[tind], sea064pld1d.ctemp[tind], zcolor = sea064pld1d.sigma0[tind], seriestype=:scatter, c=:jet, markersize = ms, markerstrokewidth = 0, legend = false, label="", clims=lims_sigma0, xlims=lims_salt, ylims=lims_temp, colorbar = false, size=(1000,1000), framestyle=:box, title="SEA064 CT vs SA")
 Plots.contour!(saltAi, ctempi, ssigma0, contour_labels=true, seriescolor = :black)
 #Plots.contour!(saltAi, ctempi, sspice0, contour_labels=true, seriescolor = :black)
 
@@ -117,7 +120,7 @@ if Plots.backend_name() != :plotly
     Plots.savefig(hMRsh1std, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_MR1000G_sh1stddev.png")
     Plots.savefig(hMRsh2std, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_MR1000G_sh2stddev.png")
     #Plots.savefig(hchla, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_FLBBCD_chla.png")
-    #Plots.savefig(hbb700, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_FLBBCD_bb700.png")
+    #Plots.savefig(h bb700, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_FLBBCD_bb700.png")
     #Plots.savefig(hcdom, figoutdir * "norse_sea064_M" * string(mission, pad=3) * "_" * region * "_FLBBCD_cdom.png")
     
     #Plots.savefig(norsephysplot, figoutdir * "norse_sea064_" * region * "_physics.png");
