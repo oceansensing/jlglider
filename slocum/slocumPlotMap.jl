3d37c91a55a2cc66df1534b95f3f22683bd9e24d
Donglai Gong <truedichotomy@gmail.com>
2024-01-04 03:03:55 -0600

    map plotting module renaming

--- slocum/slocumPlotMap.jl
+++ slocum/slocumPlotMap.jl
@@ -1,25 +1,26 @@
-# gong@vims.edu 2023-04-28
-# this script plots glider data from SEA064's NORSE project
+# gong@vims.edu 2023-12-28
+# this script plots spatial data for the slocum glider
 workdir = "/Users/gong/GitHub/jlglider/slocum/"
 if (workdir in LOAD_PATH) == false
     push!(LOAD_PATH, workdir);
 end
 
 using GLMakie, NCDatasets, NaNMath, Dates, Interpolations
-import seaexplorer_functions: seaexplorer_MR_laur_load
+#import seaexplorer_functions: seaexplorer_MR_laur_load
 
-if (@isdefined jm) == false
-    display("Loading PASSENGERS electa data.")
-    include("run_passengers_glider_2023.jl")
+if (@isdefined glider1) == false
+    display("Need to load glider data first.")
+    return
+    #include("run_passengers_glider_2023.jl")
 end
 display("PASSENGERS electa data loaded, begin plotting...")
 
-pzlist = [0, -10, -20, -30, -40, -50, -60, -80, -100, -150, -200, -300, -400, -600, -800];
+pzlist = [0, -10, -20, -30, -40, -50, -60, -80, -100, -150];
 pvarlist = ["ctemp", "saltA", "sigma0", "sndspd", "spice0"];
 
 #pzlist = [0];
-pzlist = [0, -10, -20, -30, -40, -50, -60, -80, -90, -100, -125, -150, -175, -200, -225, -250, -275, -300, -325, -350, -375, -400, -450, -500, -550, -600, -650, -700, -750, -800, -850, -900];
-pvarlist = ["spice0"]
+#pzlist = [0, -10, -20, -30, -40, -50, -60, -80, -90, -100, -125, -150, -175, -200, -225, -250, -275, -300, -325, -350, -375, -400, -450, -500, -550, -600, -650, -700, -750, -800, -850, -900];
+#pvarlist = ["spice0"]
 
 region = "GS" # GS, MAB, or ALL
 
@@ -28,41 +29,52 @@ if region == "GS"
     latmin, latmax = 37, 40;
     lonmin, lonmax = -65, -61;
 elseif region == "MAB"
-    latmin, latmax = 69, 71;
-    lonmin, lonmax = 0, 10;
+    latmin, latmax = 34.5, 43;
+    lonmin, lonmax = -77.5, -65.5;
+elseif region == "JM"
+    latmin, latmax = 37, 40;
+    lonmin, lonmax = -65, -61;
+elseif region == "LBE"
+    latmin, latmax = 34.5, 43;
+    lonmin, lonmax = -77.5, -65.5;
+elseif region == "Norwegian Sea"
+    latmin, latmax = 65, 80;
+    lonmin, lonmax = -10, 10;
 else
-    latmin, latmax = 68, 73;
-    lonmin, lonmax = -13, 17;
+    latmin, latmax = 34, 44;
+    lonmin, lonmax = -77.5, -61;
 end
 
 ms = 15;
 
-for pvar in pvarlist
-    for pz in pzlist
+#for pvar in pvarlist
+#    for pz in pzlist
+        pvar = pvarlist[1]
+        pz = pzlist[1]
         if pvar == "ctemp"
-            c1 = jm.ctemp;
-            c2 = lbe.ctemp;
-            cmin, cmax = -0.5, 9;
+            c1 = glider1.ctemp;
+            c2 = glider2.ctemp;
+            cmin, cmax = pst.tempmin, pst.tempmax;
         elseif pvar == "saltA"
-            c1 = jm.saltA;
-            c2 = lbe.saltA;
-            cmin, cmax = 34.5, 35.5;
+            c1 = glider1.saltA;
+            c2 = glider2.saltA;
+            cmin, cmax = pst.saltmin, pst.saltmax;
         elseif pvar == "sigma0"
-            c1 = jm.sigma0;
-            c2 = lbe.sigma0;
-            cmin, cmax = 27.3, 28.2;
+            c1 = glider1.sigma0;
+            c2 = glider2.sigma0;
+            cmin, cmax = pst.sigma0min, pst.sigma0max;
         elseif pvar == "sndspd"
-            c1 = jm.sndspd;
-            c2 = lbe.sndspd;
-            cmin, cmax = 1450, 1490;
+            c1 = glider1.sndspd;
+            c2 = glider2.sndspd;
+            cmin, cmax = pst.sndspdmin, pst.sndspdmax;
         elseif pvar == "spice0"
-            c1 = jm.spice0;
-            c2 = lbe.spice0;
-            cmin, cmax = -0.15, 1.2;
+            c1 = glider1.spice0;
+            c2 = glider2.spice0;
+            cmin, cmax = pst.spice0min, pst.spice0max;
         elseif pvar == "epsilon"
             if (@isdefined eps1) == false
                 #include("seaexplorer_plotMR.jl")
-                lon1, lat1, lon2, lat2, eps1, eps2 = seaexplorer_MR_laur_load(jmpld1d, lbepld1d, pz, 10.0);
+            #    lon1, lat1, lon2, lat2, eps1, eps2 = seaexplorer_MR_laur_load(jmpld1d, lbepld1d, pz, 10.0);
             end
             c1 = eps1;
             c2 = eps2;
@@ -71,31 +83,33 @@ for pvar in pvarlist
 
         plotflag = pvar * "-" * string(abs(pz)) * "m";
 
-        ptitle = "NORSE SEA064 " * region * " " * pvar * " (" * string(abs(pz)) * "m)";
+        ptitle = pst.mission * " " * pst.glidername * " " * region * " " * pvar * " (" * string(abs(pz)) * "m)";
         
-        if (region == "JM") | (region == "LBE")
-            pfname = "NORSE_SEA064_" * region * "_" * pvar * "_" * string(abs(pz)) * "m.png";
+        if region in ["JM", "LBE", "GS", "MAB"]
+            pfname = pst.mission * "_" * pst.glidername * "_" * region * "_" * pvar * "_" * string(abs(pz)) * "m.png";
         else
-            pfname = "NORSE_SEA064_" * pvar * "_" * string(abs(pz)) * "m.png";
+            pfname = pst.mission * "_" * pst.glidername * "_" * pvar * "_" * string(abs(pz)) * "m.png";
         end
 
         zlo, zhi = pz-10, pz+10;
 
-        x1 = jm.lon;
-        y1 = jm.lat;
-        z1 = jm.z;
+        x1 = glider1.lon;
+        y1 = glider1.lat;
+        z1 = glider1.z;
 
         z1ind = findall(zlo .<= z1 .<= zhi);
         pind1 = z1ind;
 
-        x2 = lbe.lon;
-        y2 = lbe.lat;
-        z2 = lbe.z;
+        x2 = glider2.lon;
+        y2 = glider2.lat;
+        z2 = glider2.z;
 
         z2ind = findall(zlo .<= z2 .<= zhi);
         pind2 = z2ind;
 
-        figoutdir = "/Users/gong/GitHub/jlglider/seaexplorer/figures/"
+        #figoutdir = "/Users/gong/GitHub/jlglider/slocum/figures/"
+        figoutdir = pst.figoutdir;
+
         bathypath = "/Users/gong/oceansensing Dropbox/C2PO/Data/bathy/ETOPO1/ETOPO_2022_v1_30s_N90W180_surface.nc";
         bathyds = Dataset(bathypath,"r");
 
@@ -105,7 +119,7 @@ for pvar in pvarlist
         # approximate x-axis scaling to make it look "normal"
         dlat = latmax - latmin;
         dlon = lonmax - lonmin;
-        lat0 = 70;
+        lat0 = NaNMath.mean([y1; y2]);
         xfac = sind(90-lat0);
         yres = 1200;
         pres = (ceil(yres * (xfac/(dlat/dlon))), yres);
@@ -115,21 +129,21 @@ for pvar in pvarlist
         lonind = findall(lonmin-0.1 .<= lon .<= lonmax+0.1);
 
         z = bathyds["z"][lonind, latind];
-        zmin, zmax = -4000, 4000;
-        z[1,1] = -4000;
-        z[end,end] = 4000;
+        zmin, zmax = -5500, 5500;
+        z[1,1] = -5500;
+        z[end,end] = 5500;
 
         x = lon[lonind];
         y = lat[latind];
 
-        fig = Figure(resolution = pres, fontsize = 32)
+        fig = Figure(size = pres, fontsize = 32)
         ax = Axis(
             fig[1, 1];
             title = ptitle,
             xlabel = "Longitude",
             ylabel = "Latitude",
         )
-        Makie.contourf!(x, y, z, xlims = (lonmin, lonmax), ylims = (latmin, latmax), levels = 128, colormap = :bukavu, colorrange = (-4000, 4000))
+        #Makie.contourf!(x, y, z, xlims = (lonmin, lonmax), ylims = (latmin, latmax), levels = 128, colormap = :bukavu, colorrange = (-4000, 4000))
         if pvar != "epsilon"
             if region != "LBE"
                 Makie.scatter!(x1[pind1], y1[pind1], color = c1[pind1], colormap=:jet, markersize=ms, colorrange=(cmin, cmax))
@@ -154,11 +168,11 @@ for pvar in pvarlist
         end
         Colorbar(fig[1, 2], limits = (cmin, cmax), colormap = :jet, flipaxis = false)
         fig
-        save(figoutdir * pfname, fig)
-    end #pzlist
-end #pvarlist
+        #save(figoutdir * pfname, fig)
+#    end #pzlist
+#end #pvarlist
 
-lon1, lat1, lon2, lat2, eps1, eps2 = seaexplorer_MR_laur_load(jmpld1d, lbepld1d, -10, 10.0);
+#lon1, lat1, lon2, lat2, eps1, eps2 = seaexplorer_MR_laur_load(jmpld1d, lbepld1d, -10, 10.0);
 
 #= 
 fig = Figure()
