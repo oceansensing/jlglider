56e590f5a19271e9bc8bd0201e82e529d8cb178f
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2024-01-13 00:23:15 -0500

    added data paths for 2023 NORSE deployment

--- microrider/mr_p2mat.m
+++ microrider/mr_p2mat.m
@@ -1,10 +1,11 @@
 % This matlab script runs odas_p2mat and converts all P files into MAT files
 % gong@vims.edu 2023-04-18
 
-datadirJM = '/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221021-norse-janmayen-complete/mr1000g/';
-datadirLBE = '/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221102-norse-lofoten-complete/mr1000g/';
+datadirJM2022 = '/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221021-norse-janmayen-complete/mr1000g/';
+datadirLBE2022 = '/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20221102-norse-lofoten-complete/mr1000g/';
+datadirJM2023 = '/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/sea064-20231127-norse-complete/mr/';
 
-datadir = datadirJM
+datadir = datadirJM2023
 datafiles = dir([datadir '*.p']);
 
 for ii = 1:length(datafiles)
