1da2ea71c8a6725d06f646005445d7af3167fd61
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2024-01-14 15:34:41 -0500

    Create moov.jl
    The moov function moves files matching a specified stringpattern from fromdir to todir.

---
+++ microrider/moov.jl
@@ -0,0 +1,16 @@
+
+# the moov function moves files matching a specified string pattern from fromdir to todir
+# Donglai Gong, 2024-01-14 : initial write
+
+using Glob
+function moov(stringpattern::String, fromdir::String, todir::String)
+    frompath = readdir(Glob.GlobMatch(stringpattern), fromdir);
+    topath = joinpath.(todir, basename.(frompath));
+    mv.(frompath, topath, force=true);
+end
+
+# example usage:
+#stringpattern = "*.txt";
+#fromdir = "director1";
+#todir = "directory2";
+#moov(stringpattern, fromdir, todir)
