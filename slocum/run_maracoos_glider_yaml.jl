be9b55efa3056bf62f1de957124260150e893a08
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2024-09-09 21:41:04 -0400

    updating the run script to move section plots into a function and added a map plot

--- slocum/run_maracoos_glider_yaml.jl
+++ slocum/run_maracoos_glider_yaml.jl
@@ -4,7 +4,7 @@
 #
 
 using PyCall
-using Glob, YAML, NaNMath, Statistics, GibbsSeaWater, Dates, Interpolations, YAML, JLD2
+using Glob, YAML, NaNMath, Statistics, GibbsSeaWater, Dates, Interpolations, YAML, JLD2, NCDatasets, GLMakie
 
 include("slocumType.jl")
 include("slocumFunc.jl")
@@ -15,14 +15,14 @@ using .slocumType: plotSetting, plotStruct, ctdStruct, sciStruct
 #using Main.slocumLoad.slocumType: ctdStruct
 import .slocumFunc: pyrow2jlcol, intersectalajulia2, glider_var_load, glider_presfunc
 import .slocumLoad: load_glider_ctd, load_glider_sci, glider_ctd_qc, slocumYAMLload
-import .slocumPlot: plot_glider_ctd
+import .slocumPlot: plot_glider_ctd, plotSlocumCTD
 
-reloadflag = true
+reloadflag = false
 
 gliderdatadir = "/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/"; 
 missionYAMLdir = "/Users/gong/GitHub/jlglider/slocum/mission_yaml/";
 
-if @isdefined(gliderCTDarray) == false
+if @isdefined(gliderCTDarray)
     if reloadflag == true
         gliderCTDarray = slocumYAMLload(missionYAMLdir);
         jldsave(gliderdatadir * "slocumCTDdata.jld2"; gliderCTDarray);
@@ -33,48 +33,67 @@ if @isdefined(gliderCTDarray) == false
     end
 end
 
+#plotSlocumCTD(gliderCTDarray)
 
-for i = 1:length(gliderCTDarray)
-    #i = 8
-    display(i)
-    gliderCTDraw = gliderCTDarray[i];
-    lonrange = [NaNMath.minimum(gliderCTDraw.lon) NaNMath.maximum(gliderCTDraw.lon)];
-    latrange = [NaNMath.minimum(gliderCTDraw.lat) NaNMath.maximum(gliderCTDraw.lat)]; 
+bathypath = "/Users/gong/oceansensing Dropbox/C2PO/Data/bathy/ETOPO1/ETOPO_2022_v1_30s_N90W180_surface.nc";
+bathyds = Dataset(bathypath,"r");
 
-    pint = 1; # this is the data decimation for plotting. Makie is so fast that it's not necessary, but Plots.jl would need it. Not using Plots.jl because of a bug there with colormap
-    iday = 1; # day intervals for plotting
-    ms = 6; # marker size
-    tsms = 6; # time series marker size
-    pres = (1200, 800); # plot resolution
-    tspres = (1000, 1000); # time series plot resolution
-    fs = 24; # font size
-    ps = plotSetting(pint, iday, ms, tsms, pres, tspres, fs);
+lon = bathyds["lon"][:];
+lat = bathyds["lat"][:];
 
-    ctempstd = std(gliderCTDraw.ctemp);
-    condstd = std(gliderCTDraw.cond);
-    saltAstd = std(gliderCTDraw.saltA);
-    sigma0std = std(gliderCTDraw.sigma0);
-    sndspdstd = std(gliderCTDraw.sndspd);
-    spice0std = std(gliderCTDraw.spice0);
+latmin, latmax = 35, 42;
+lonmin, lonmax = -77.5, -69;
 
-    nsig = 2.5
+# approximate x-axis scaling to make it look "normal"
+dlat = latmax - latmin;
+dlon = lonmax - lonmin;
+lat0 = NaNMath.mean([38.0]);
+xfac = sind(90-lat0);
+yres = 2000;
+pres = (ceil(yres * (xfac/(dlat/dlon))), yres);
 
-    #figoutdir = "/Users/gong/GitHub/jlglider/slocum/figures/";
-    figoutdir = "/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/figures/";
-    ctemprange = (NaNMath.mean(gliderCTDraw.ctemp) .- nsig*ctempstd, NaNMath.mean(gliderCTDraw.ctemp) .+ nsig*ctempstd);
-    condrange = (NaNMath.mean(gliderCTDraw.cond) .- nsig*condstd, NaNMath.mean(gliderCTDraw.cond) .+ nsig*condstd);
-    saltArange = (NaNMath.mean(gliderCTDraw.saltA) .- nsig*saltAstd, NaNMath.mean(gliderCTDraw.saltA) .+ nsig*saltAstd);
-    sigma0range = (NaNMath.mean(gliderCTDraw.sigma0) .- nsig*sigma0std, NaNMath.mean(gliderCTDraw.sigma0) .+ nsig*sigma0std);
-    sndspdrange = (NaNMath.mean(gliderCTDraw.sndspd) .- nsig*sndspdstd, NaNMath.mean(gliderCTDraw.sndspd) .+ nsig*sndspdstd);
-    spice0range = (NaNMath.mean(gliderCTDraw.spice0) .- nsig*spice0std, NaNMath.mean(gliderCTDraw.spice0) .+ nsig*spice0std);
-    temprange = ctemprange;
-    saltrange = saltArange;
-    pst = plotStruct(figoutdir, gliderCTDraw.mission, gliderCTDraw.glidername, temprange[1], temprange[2], condrange[1], condrange[2], saltrange[1], saltrange[2], sigma0range[1], sigma0range[2], spice0range[1], spice0range[2], sndspdrange[1], sndspdrange[2]);
+# extract indices from the bathymetric data file
+latind = findall(latmin-0.1 .<= lat .<= latmax+0.1);
+lonind = findall(lonmin-0.1 .<= lon .<= lonmax+0.1);
 
-    plot_glider_ctd(gliderCTDraw, ps, pst);
+z = Float64.(bathyds["z"][lonind, latind]); # recasting as Float64 to fix a StackOverFlow error seen in GLMakie 0.6.0.
+x = lon[lonind];
+y = lat[latind];
+
+pzind = findall(z .> 1);
+nzind = findall(z .< -1);
+zzind = findall(-1 .<= z .<= 1);
+
+log10z = deepcopy(z);
+log10z[pzind] .= log10.(z[pzind]);
+log10z[nzind] .= -log10.(-z[nzind]);
+log10z[zzind] .= 0;
+
+fig = Figure(size = pres, fontsize = 54)
+ax = Axis(
+    fig[1, 1];
+    title = "SMAB MARACOOS Glider Deployments (" * string(year(unix2datetime(NaNMath.minimum(gliderCTDarray[1].t)))) * "--" * string(year(unix2datetime(NaNMath.maximum(gliderCTDarray[end].t)))) * ")",
+    xlabel = "Longitude",
+    ylabel = "Latitude",
+)
+Makie.contourf!(x, y, log10z, colormap = :bukavu, levels = range(-6., 6., length = 128))
+xlims!(lonmin, lonmax);
+ylims!(latmin, latmax);
+
+for ii = 1:length(gliderCTDarray)
+    display(ii)
+    Makie.scatter!(
+        gliderCTDarray[ii].lon, 
+        gliderCTDarray[ii].lat, 
+        color = gliderCTDarray[ii].t, 
+        colorrange = (minimum(gliderCTDarray[1].t), maximum(gliderCTDarray[end].t)), 
+        colormap=:heat, 
+        markersize=8
+        )
 end
-
-#include("slocumLoadTest.jl")
+fig
+save("/Users/gong/oceansensing Dropbox/C2PO/glider/gliderData/figures/SMAB_MARACOOS_glider.png", fig)
+GLMakie.closeall()
 
 #=pst = pst_electa;
 glider1 = electaCTDraw;
