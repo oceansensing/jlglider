9456a6e2dd1bb9f1ed5b1a0974b82819272d2f57
Donglai Gong <15986815+truedichotomy@users.noreply.github.com>
2025-03-23 14:34:43 -0400

    Separated the Glider.jl type module for slocum and seaexplorer so the subdirectories are not cross dependent

--- seaexplorer/Glider.jl
+++ seaexplorer/Glider.jl
@@ -1,8 +1,6 @@
 module Glider
     include("/Users/gong/GitHub/jlglider/seaexplorer/seaexplorerType.jl")
-    include("/Users/gong/GitHub/jlglider/seaexplorer/gliderPlotType.jl")
-    include("/Users/gong/GitHub/jlglider/slocum/slocumType.jl")
+    include("/Users/gong/GitHub/jlglider/plotting/gliderPlotType.jl")
     import .seaexplorerType: NAV_RT, PLD_RT, SeaExplorerData, SeaExplorerCTD, LEGATO
-    import .slocumType: engStruct, ctdStruct, sciStruct
     import .gliderPlotType: plotSetting, plotStruct
 end
---
+++ slocum/Glider.jl
@@ -0,0 +1,6 @@
+module Glider
+    include("/Users/gong/GitHub/jlglider/slocum/slocumType.jl")
+    include("/Users/gong/GitHub/jlglider/plotting/gliderPlotType.jl")
+    import .slocumType: engStruct, ctdStruct, sciStruct
+    import .gliderPlotType: plotSetting, plotStruct
+end
